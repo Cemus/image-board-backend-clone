97745c2d849b46d2155ea53274432bfcbb555c8c
Cemus <24947582+Cemus@users.noreply.github.com>
2023-10-10 12:30:37 +0200

    fix storage

--- utils/threadPurge.js
+++ utils/threadPurge.js
@@ -3,8 +3,15 @@ import { Thread } from "../models/threadModel.js";
 const threadPurge = async () => {
   const maxThreads = 16;
   const allThreads = await Thread.find({}).sort({ bumpDate: 1 });
-  while (allThreads.length > maxThreads) {
-    await Thread.findByIdAndDelete(allThreads[0]._id);
+
+  if (allThreads.length > maxThreads) {
+    const threadsToDelete = allThreads.slice(0, allThreads.length - maxThreads);
+
+    const deletedThreads = await Thread.deleteMany({
+      _id: { $in: threadsToDelete.map((thread) => thread._id) },
+    });
+
+    console.log(`${deletedThreads.deletedCount} threads supprimés.`);
   }
 };
 
