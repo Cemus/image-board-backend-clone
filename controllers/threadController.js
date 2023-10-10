8511e8e5811595cadcb6e88b5176c7fee72c9c3d
Cemus <24947582+Cemus@users.noreply.github.com>
2023-10-10 10:19:26 +0200

    fix storage

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -52,6 +52,10 @@ const createThread = async (req, res) => {
   const metadata = await getImageMetadata(imageBuffer);
   const { width, height } = metadata;
   fs.readFile(imageKey, (error, data) => {
+    if (error) {
+      console.error("Erreur lors de la lecture du fichier :", error);
+      return;
+    }
     console.log(data);
     const json = JSON.parse(data);
     console.log("json :");
